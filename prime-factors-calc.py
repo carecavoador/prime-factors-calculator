7b7e96dedad8e4624a7c3fb884b9c4628846ed57
Dennys Azevedo <dennys.bd@gmail.com>
2019-01-02 20:27:20 -0300

    prime_factors only

--- prime-factors-calc.py
+++ prime-factors-calc.py
@@ -2,35 +2,31 @@ import datetime
 import math
 
 
-# This function calculates all the possible factors for the given number.
-def find_number_factors(factored_number):
+# This function calculates all the prime factors for the given number.
+def prime_factors(factored_number):
     iterable = []
+
     if factored_number > 2:
         iterable.append(2)
-        initial_factor = int(factored_number/2 + 1)
-        iterable.extend(list(range(3,initial_factor,2)))
-    return [i for i in iterable if factored_number % i == 0]
 
-# This function receives a number and a list of its factors, checks wich of the
-# given factors are prime numbers, than adds them to prime_factors_list.
-def find_prime_factors(factor_list):
-    return [i for i in factor_list if len(find_number_factors(i)) == 0]
+    initial_factor = int(math.sqrt(factored_number))
+    iterable.extend(list(range(3,initial_factor,2)))
 
-def find_prime_factorization(factored_number, prime_list):
+    result = [i for i in iterable if factored_number % i == 0 and len(prime_factors(i)) == 0]
+    reverse = [int(factored_number / i) for i in result if factored_number / i > initial_factor and len(prime_factors(factored_number / i)) == 0 ]
+    result.extend(reversed(reverse))
+
+    return result
+
+
+def prime_factorization(factored_number, prime_list):
     factorization = []
     for i in prime_list:
         if factored_number % i == 0:
             factorization.append(i)
-            factorization.extend(find_prime_factorization((factored_number / i), prime_list))
+            factorization.extend(prime_factorization((factored_number / i), prime_list))
             break
     return factorization
-
-def test():
-    l = []
-    for i in range(100000):
-        if not i & 1:
-            l.append(i)
-    return l
 
 # This function's purpose is to generate a human friendly factorization string.
 def beauty_factors(factor_list):
@@ -69,20 +65,18 @@ log_file.write('On %s\nTested number was: %d' % (
 print('-> Finding prime factors...')
 
 
-tested_number_factor_list = find_number_factors(tested_number)
-print(f'function 1: {tested_number_factor_list}' )
+primes = prime_factors(tested_number)
 
 
-if len(tested_number_factor_list) == 0:
+if len(primes) == 0:
     print(f'-> {tested_number} is a prime number')
     log_file.write('\n%d is a prime number' % (tested_number))
 else:
     log_file.write('\nPrime factors found:')
-    prime_factors = find_prime_factors(tested_number_factor_list)
-    for i in prime_factors:
+    for i in primes:
         print(f"   -> {i} is a prime factor")
         log_file.write(f'\n    {i}')
-    factorization = find_prime_factorization(tested_number, prime_factors)
+    factorization = prime_factorization(tested_number, primes)
     print(factorization)
     print('-> Prime factorization: %s' % (beauty_factors(factorization)))
     log_file.write('\nPrime factorization: %s' % beauty_factors(factorization))
